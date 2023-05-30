32f99fa2018aa324fa373ae4918707c8ae6dbadd
juaniM4c <juanimachuca1@gmail.com>
2023-05-30 15:26:22 -0300

    elimino la prueba

--- clases/prueba.py
+++
@@ -1,6 +0,0 @@
-from EntradaDatos import EntradaDatos
-entrada_datos1 = EntradaDatos("juani","machuca","j@email.com",150)
-print(entrada_datos1)
-
-entrada_datos2 = EntradaDatos("lucas","mendez","l@email.com",2560)
-print(entrada_datos2)
