f3e81fb231447475675d35707d201b49dc47bef4
NadiaGonzalez27 <nadiagonzalez206@gmail.com>
2023-06-29 10:13:34 -0300

    Update gastos_varios V3
    Agregamos la documentación de la clase

--- clases/Gastos_varios.py
+++ clases/Gastos_varios.py
@@ -1,17 +1,21 @@
 from clases.Gasto import Gasto
 
+# Creamos la clase gastos_varios que hereda de la clase padre Gasto
 class Gastos_varios(Gasto):
+
+    # Inicializamos el objeto y le pasamos los parámetros
+    # Los parámetros los utilizamos para establecer las variables de instancia
     def __init__(self, detalle, _monto_gastos_varios):
         self._detalle = detalle
-        self._monto_gastos_varios = _monto_gastos_varios
+        self._monto_gastos_varios = monto_gastos_varios
 
-
+    # El método __str__ proporciona una cadena que incluye los detalles del gasto y el monto
     def __str__(self):
         return f"-> Gastos varios\n" \
                f"\n- Detalle de la compra: {self._detalle} " \
                f"\n- Monto: ${self._monto_gastos_varios}"
 
-
+    # Método que llama al __str__ y devuelve su resultado
     def mostrar_detalles(self):
         return self.__str__()
 
